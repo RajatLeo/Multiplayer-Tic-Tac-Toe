3b0cc27757ec92ec394d54cf252142e3bb54beed
Rajat Srivastava <67239119+RajatLeo@users.noreply.github.com>
2020-07-16 23:52:07 +0530

    Update Network.py

--- Network.py
+++ Network.py
@@ -4,7 +4,7 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.43.195"
+        self.server = "Enter Your Server Address"
         self.port = 9999
         self.addr = (self.server, self.port)
         self.p = self.connect()
